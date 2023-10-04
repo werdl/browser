71fc9ab0366f9012932b731fe9be8686a917423f
werdl <116349156+werdl@users.noreply.github.com>
2023-10-04 10:44:52 +0000

    save

--- src/main.v
+++ src/main.v
@@ -2,19 +2,12 @@ module main
 
 import request
 import render
-import os
 fn main() {
 	x:=request.new("https://browser.engineering")
 	r:=x.read()
 	r.print()
 
 	
-	e:=render.parse(r.body)
-	print(e)
-	e.print()
-
-	for {
-		inp:=os.input("> ")
-		println(render.linkn(inp.int()))
-	}
+	e:=render.new(r.body)
+	e.show()
 }
--- src/modules/render/render.v
+++ src/modules/render/render.v
@@ -1,73 +1,26 @@
 module render
 
-import request
+import net.html
 
-
-
-type Elements = [][]string
-pub fn parse(body string) Elements{
-	mut text, mut in_tag:="", false
-
-	mut p:=[][]string{}{}
-	mut current:=[]string{}
-	mut i:=0
-	for c in body.runes() {
-		if c==`<`{
-			in_tag=true
-			// current=[]
-			if text!="" && body.runes()[i+1]==`/`{
-				current << text
-			} else {
-				current=[]
-			}
-			text=""
-		} else if c==`>` {
-			in_tag=false
-			current << text
-			p << current
-			
-			text=""
-		} else {
-			text+=c.str()
-		}
-		i+=1
-	}
-	if !in_tag && text!="" {
-		current << text
-	}
-	p << current
-
-	mut fresh:=[][]string{}{}
-	for q in p {
-
-		if q.len<3 || (q[0][0]==`/` && q[2][0]==`/`) || "/head" in q{
-			
-		} else {
-			fresh << q
-		}
-	}
-	return fresh
+struct DOM {
+	dom html.DocumentObjectModel
+	data string
 }
-__global (
-	links Elements
-)
-pub fn linkn(n int) []string {
-	return links[n]
+
+pub fn new(data string) DOM {
+	return DOM{
+		dom: html.parse(data)
+		data: data
+	}
 }
-pub fn (e Elements) print() {
-	for element in e {
-		if !element[0].contains("style") {
-			if element[0]=="title" {
-				println("Page Title: ${element[1]} \n\n")
-			} else if element[0].contains("href") {
-				links << element
-				$if linux {
-					println("${links.len-1} \e[1;36m${element[1]}\e[0m")
-				}
-			}
-			else {
-				println(element[1])
-			}
-		}
+
+pub fn (d DOM) show() {
+	tags_to_show:=["p","h1","h2","h3","h4","h5","h6"]
+	tags:=d.dom.get_tags(html.GetTagsOptions{})
+	for tag in tags{
+		tag_type:=tag.str().split(" ")[0]
+		tag_type.replace("<","")
+		tag_type.replace(">","")
+		println(tag_type)
 	}
 }
