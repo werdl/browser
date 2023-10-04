12c5d91194b3ea492e271c7d3832832a9c7cf384
werdl <116349156+werdl@users.noreply.github.com>
2023-10-04 18:22:44 +0100

    change

--- src/main.v
+++ src/main.v
@@ -2,12 +2,35 @@ module main
 
 import request
 import render
-fn main() {
-	x:=request.new("https://example.com")
+import os
+
+fn gen_list(max int) []string {
+	mut total:=[]string{}
+	for i in 0..max+1 {
+		total << i.str()
+	}
+	return total
+}
+fn read_site(s string) {
+	x:=request.new(s)
 	r:=x.read()
 	r.print()
 
 	
 	e:=render.new(r.body)
-	e.show()
+	max, hrefs:=e.show()
+	for {
+		inp:=os.input("> ")
+		list:=gen_list(max)
+		if inp in list{
+			read_site(hrefs[inp.int()])
+		} else if inp=="q" {
+			exit(0)
+		}else {
+			read_site(inp)
+		}
+	}
+}
+fn main() {
+	read_site("https://browser.engineering")
 }
--- src/modules/render/render.v
+++ src/modules/render/render.v
@@ -1,6 +1,8 @@
 module render
 
 import net.html
+import term
+
 
 struct DOM {
 	dom html.DocumentObjectModel
@@ -14,10 +16,44 @@ pub fn new(data string) DOM {
 	}
 }
 
-pub fn (d DOM) show() {
-	tags_to_show:=["p","h1","h2","h3","h4","h5","h6","a"]
+fn text_tag(tag html.Tag) string {
+	// tags_to_show:=["p","h1","h2","h3","h4","h5","h6","a"]
+	dont_show:=["style","meta","script","title"]
+	if tag.children.len==0 && tag.name !in dont_show {
+		x:=tag.text()
+		mut in_amp:=false
+		mut ret:=""
+		for c in x.runes() {
+			if c==`&` {
+				in_amp=true
+			} else if c==`;` {
+				in_amp=false
+			} else if !in_amp {
+				ret+=c.str()
+			}
+		}
+		return ret
+	}
+	return ""
+}
+
+struct RGB {
+	r int=255
+	g int=255
+	b int=255
+}
+
+fn prnt(rgb RGB, s string) {
+	print(term.rgb(rgb.r,rgb.g,rgb.b,s))
+}
+
+pub fn (d DOM) show() (int, []string) {
+	
 	tags:=d.dom
+	mut hrefs:=[]string{}
 	// println(d.dom)
+	mut max:=0
+	mut title:=false
 	for tag in d.dom.get_tags() {
 		// println(tag.text)
 		// split:=tag.str().split(" ")
@@ -32,8 +68,29 @@ pub fn (d DOM) show() {
 		// tag_type
 		// tag_type
 		// println(tag.text())
-		if tag.children.len==0 && tag.name in tags_to_show{
-			println("${tag.text()}")
+		match tag.name {
+			"a" {
+				print("\n${max} ")
+				prnt(RGB{
+					r: 0
+					b: 139
+					g: 139
+				}, "${tag.text()}\n")
+				max+=1
+				hrefs << tag.attributes["href"]
+			}
+			"title" {
+				if !title {
+					print("Page title: ${tag.text()}\n")
+					title=true
+				}
+				
+			}
+			else {
+				print("${text_tag(tag)}")
+			}
 		}
+		
 	}
+	return max, hrefs
 }
